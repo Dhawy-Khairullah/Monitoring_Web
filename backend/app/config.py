8faa7f02ae3be0eaac47097de47cd7a335b13854
Dhawy-Khairullah <dhawykhairullah@gmail.com>
2025-06-04 14:33:58 +0700

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,6 @@ load_dotenv(dotenv_path=env_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
-google_drive_json_relative = os.getenv("GOOGLE_DRIVE_JSON")
-GOOGLE_DRIVE_JSON = (Path(__file__).resolve().parent.parent / google_drive_json_relative).resolve()
+GOOGLE_DRIVE_JSON_STRING = os.getenv("GOOGLE_DRIVE_JSON")
+GOOGLE_DRIVE_JSON = (Path(__file__).resolve().parent.parent / GOOGLE_DRIVE_JSON_STRING).resolve()
 GOOGLE_DRIVE_FOLDER_ID = os.getenv("GOOGLE_DRIVE_FOLDER_ID")
