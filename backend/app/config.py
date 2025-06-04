a98bf6cd245c188cc367f357dc4d6d0f2f4d3653
Dhawy-Khairullah <dhawykhairullah@gmail.com>
2025-06-04 14:35:32 +0700

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -7,6 +8,6 @@ load_dotenv(dotenv_path=env_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
-GOOGLE_DRIVE_JSON_STRING = os.getenv("GOOGLE_DRIVE_JSON")
+GOOGLE_DRIVE_JSON = json.loads(GOOGLE_DRIVE_JSON_STRING)
 GOOGLE_DRIVE_JSON = (Path(__file__).resolve().parent.parent / GOOGLE_DRIVE_JSON_STRING).resolve()
 GOOGLE_DRIVE_FOLDER_ID = os.getenv("GOOGLE_DRIVE_FOLDER_ID")
